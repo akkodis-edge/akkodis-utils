79b6276589f7759a32637578fb177972c551593e
Mikko Salomaki <mikko.salomaki@akkodis.com>
2026-09-23 11:00:00 +0200

    owl: allow dumping available sensors in db

--- owl.py
+++ owl.py
@@ -37,6 +37,10 @@ LIBOWL_OP_LESS_THAN = 2
 LIBOWL_OP_LESS_EQUAL = 3
 LIBOWL_OP_EQUAL = 4
 
+sensor_type_name = {
+    SENSOR_TEMPERATURE: 'TEMP',
+}
+
 class LibOwl:
     def __init__(self, path):
         self.lib = cdll.LoadLibrary("libowl.so")
@@ -48,6 +52,31 @@ class LibOwl:
     def __del__(self):
         if (self.owl):
             self.lib.libowl_close(self.owl)
+    def list_sensors(self):
+        c_data_array_type = LibOwlSensorData * 1
+        c_data_array = c_data_array_type()
+        c_filter_array_type = LibOwlFilter * 1
+        c_filter_array = c_filter_array_type()
+        last_name = c_char_p(b'')
+        sensors = []
+        while True:
+            ret = self.lib.libowl_filter_name(byref(c_filter_array[0]), c_int(LIBOWL_OP_GREATER_THAN), last_name)
+            if (ret != 0):
+                raise OSError(ret, os.strerror(ret), 'Failed creating filter')
+            try:
+                ret = self.lib.libowl_read(self.owl, c_int(0), byref(c_filter_array), c_size_t(len(c_filter_array)),
+                                                byref(c_data_array), c_size_t(len(c_data_array)))
+                if ret < 0:
+                     raise OSError(ret, os.strerror(ret), 'Failed reading db')
+                if ret > 0:
+                    last_name = c_data_array[0].name
+                    sensors.append((last_name.decode('utf-8'), c_data_array[0].type))
+                if ret == 0:
+                    break
+            finally:
+                self.lib.libowl_sensor_data_free(byref(c_data_array[0]))
+        return sensors
+
     def read(self, limit, after=None, before=None):
         # create filters
         filters = []
@@ -90,6 +119,10 @@ def main():
     args = parser.parse_args()
 
     db = LibOwl(args.db)
+
+    print('Available sensors in db:')
+    for (name, type) in db.list_sensors():
+        print('({}): {}'.format(sensor_type_name.get(type, 'UNKNOWN'), name))
 
     next_epoch = 0.0
     while True:
